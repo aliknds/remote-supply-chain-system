79b8767269a3b72d95eb5d0fae5f5246dd4c6bde
alikghmn <alik_mn@outlook.com>
2023-06-30 13:54:58 +0800

    manage cursor better

--- javascript.js
+++ javascript.js
@@ -28,12 +28,13 @@ class Table {
     handleInput = (event) => {
         if (this.isEditableCell(event.target)) {
             if (event.target.classList.contains('unit-price')) {
+                const selection = this.saveSelection(event.target);
                 const newUnitPrice = parseFloat(event.target.innerText) || this.defaultUnitPrice;
                 const unitPriceCells = this.table.querySelectorAll('.unit-price');
                 unitPriceCells.forEach(cell => {
                     cell.innerText = newUnitPrice.toFixed(2);
-                    this.setCursorToEnd(cell);
                 });
+                this.restoreSelection(event.target, selection);
             }
             this.updateTable();
         }
@@ -58,9 +59,10 @@ class Table {
             let unitPrice = parseFloat(unitPriceCell.innerText);
 
             if (!unitPrice) {
+                const selection = this.saveSelection(unitPriceCell);
                 unitPriceCell.innerText = this.defaultUnitPrice;
                 unitPrice = this.defaultUnitPrice;
-                this.setCursorToEnd(unitPriceCell);
+                this.restoreSelection(unitPriceCell, selection);
             }
 
             if (!quantity) {
@@ -80,11 +82,55 @@ class Table {
         return node.tagName === 'TD' && node.contentEditable === 'true';
     }
 
-    setCursorToEnd(el) {
+    saveSelection(element) {
+        const selection = window.getSelection();
+        if (selection.rangeCount > 0) {
+            const range = selection.getRangeAt(0);
+            const preSelectionRange = range.cloneRange();
+            preSelectionRange.selectNodeContents(element);
+            preSelectionRange.setEnd(range.startContainer, range.startOffset);
+            const start = preSelectionRange.toString().length;
+
+            return {
+                start: start,
+                end: start + range.toString().length
+            };
+        } else {
+            return null;
+        }
+    }
+
+    restoreSelection(element, savedSel) {
+        let charIndex = 0;
         const range = document.createRange();
+        range.setStart(element, 0);
+        range.collapse(true);
+        const nodeStack = [element];
+        let node;
+        let foundStart = false;
+        let stop = false;
+
+        while (!stop && (node = nodeStack.pop())) {
+            if (node.nodeType === 3) {
+                const nextCharIndex = charIndex + node.length;
+                if (!foundStart && savedSel.start >= charIndex && savedSel.start <= nextCharIndex) {
+                    range.setStart(node, savedSel.start - charIndex);
+                    foundStart = true;
+                }
+                if (foundStart && savedSel.end >= charIndex && savedSel.end <= nextCharIndex) {
+                    range.setEnd(node, savedSel.end - charIndex);
+                    stop = true;
+                }
+                charIndex = nextCharIndex;
+            } else {
+                let i = node.childNodes.length;
+                while (i--) {
+                    nodeStack.push(node.childNodes[i]);
+                }
+            }
+        }
+
         const sel = window.getSelection();
-        range.selectNodeContents(el);
-        range.collapse(false);
         sel.removeAllRanges();
         sel.addRange(range);
     }
